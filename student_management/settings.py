41ad313ed6946ea62f07421bff8805aa200ca3fa
Kanagasundar <kanagasundar1996@gmail.com>
2024-08-13 20:24:27 +0530

    updated commits

--- student_management/settings.py
+++ student_management/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*i#luz_z5typhdyh1l=-*c1a4&sjncr9+f6nbhca*_6n&(vu(*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
